834593a0128319a3c2cb8de2767c1a10903be361
Noeline <noeline.mar@onja.org>
2021-07-31 11:59:22 +0300

    Remove unused files

--- config/index.js
+++ config/index.js
@@ -50,7 +50,6 @@ module.exports = {
     },
   ],
 
-  //-- NAVIGATION SETTINGS -----
   navLinks: {
     menu: [
       {
--- cypress/e2e/main.spec.js
+++ cypress/e2e/main.spec.js
@@ -19,34 +19,6 @@ describe("IndexPage", () => {
     cy.findByTestId("animated-heading").should("have.css", "opacity", "1")
   })
 
-  it("references legal pages", () => {
-    cy.findByTestId("footer-links")
-      .findByText(/imprint/i)
-      .should("have.attr", "href")
-      .and("include", "imprint")
-    cy.findByTestId("footer-links")
-      .findByText(/privacy/i)
-      .should("have.attr", "href")
-      .and("include", "privacy")
-  })
-
-  it("renders other pages", () => {
-    cy.get("#layout-wrapper").then(elem => {
-      if (elem.attr("data-usecookiebar") === "true") {
-        cy.get("button#confirm").click()
-        cy.findByTestId("footer-links")
-          .findByText(/imprint/i)
-          .click()
-        cy.findByTestId("heading").should("exist")
-      } else {
-        cy.findByTestId("footer-links")
-          .findByText(/imprint/i)
-          .click()
-        cy.findByTestId("heading").should("exist")
-      }
-    })
-  })
-
   it("renders cookie bar if activated", () => {
     cy.get("#layout-wrapper").then(elem => {
       if (elem.attr("data-usecookiebar") === "true") {
